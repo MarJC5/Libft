df8c912e7aaa8a1c483bd76696558eecb435e6a2
Martin Jean-Christio <admin@marjc.ch>
2021-10-14 22:04:13 +0200

    Update

--- libc/ft_strlcat.c
+++ libc/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: jmartin <jmartin@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/14 15:54:44 by jmartin           #+#    #+#             */
-/*   Updated: 2021/10/14 17:08:32 by jmartin          ###   ########.fr       */
+/*   Updated: 2021/10/14 21:58:05 by jmartin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,22 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t n)
 {
+	size_t	i;
+	size_t	j;
 	size_t	dst_len;
 	size_t	src_len;
 
 	dst_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
+	i = ft_strlen(dst);
+	j = 0;
 	if (n < dst_len + 1)
-		return (dst_len + n);
+		return (src_len + n);
 	else if (n > dst_len + 1)
 	{
-		ft_memcpy(dst, src, n - 1);
-		dst[n - 1] = '\0';
+		while (i < n - 1)
+			*(dst + i++) = *(src + j++);
+		*(dst + i) = '\0';
 	}
 	return (dst_len + src_len);
 }
